169032792f195ab33391a8199956bb6ebe7d1e0b
Abnesvir <Abnesvir@gmail.com>
2019-10-29 11:01:55 +0800

    Site updated: 2019-10-29 11:01:49

--- js/matery.js
+++ js/matery.js
@@ -104,6 +104,12 @@ $(function () {
             selector: '.img-item',
             // 启用字幕
             subHtmlSelectorRelative: true
+            showThumbByDefault: false
+        });
+
+        // 图片懒加载
+        $(document).find('img[data-original]').each(function(){
+            $(this).parent().attr("href", $(this).attr("data-original"));
         });
 
         // progress bar init
